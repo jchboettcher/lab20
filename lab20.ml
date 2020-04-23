b83cabf4d9f3f9637cfdf65d9f85d8617dc6facd
Jack Boettcher <jcboettcher@college.harvard.edu>
2020-04-23 12:11:25 -0500

    done

--- lab20.ml
+++ lab20.ml
@@ -1,40 +1,41 @@
-type image = float list list ;;
+type imag = float list list ;;
 (* images are lists of lists of floats between 0. (white) and 1. (black) *)
-type size = int * int ;;
 open Graphics ;;
-  
+
 (* threshold thershold image -- image where pixels above the threshold
 value are black *)
-let threshold img threshold =
-  List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
-                                 row) img
-       
-(* show the image *)
-let depict img =
-  Graphics.open_graph ""; Graphics.clear_graph ();
-  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
-  let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
-  List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
-  Unix.sleep 2; Graphics.close_graph () ;;
+let threshold (img : imag) (threshold : float) : imag =
+  let f v = if v <= threshold
+    then 0.
+    else 1. in
+  List.map (fun row -> List.map f row) img
 
 (* dither max image -- dithered image *)
-let dither img =
-  List.map
-    (fun row ->
-     List.map
-       (fun v -> if v > Random.float 1.
-                 then 1.
-                 else 0.) row)
-    img
-    
+let dither (img : imag) : imag  =
+  let f v = if v > Random.float 1.
+    then 1.
+    else 0. in
+  List.map (fun row -> List.map f row) img
+
+(* show the image *)
+let depict (img : imag) : unit =
+  Graphics.open_graph "";
+  Graphics.clear_graph ();
+  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
+  let depict_pix (pix : float) (row : int) (column : int) : unit =
+    let lvl = int_of_float (255. *. (1. -. pix)) in
+    Graphics.set_color (Graphics.rgb lvl lvl lvl);
+  plot column (y - row) in
+  List.iteri (fun row row' -> List.iteri (fun column pix -> depict_pix pix row column) row') img;
+  Unix.sleep 2;
+  Graphics.close_graph () ;;
+
 let mona = Monalisa.image ;;
-  
-  depict mona ;;
-    
-  let mona_threshold = threshold mona 0.75 ;;
-    depict mona_threshold ;;
-      
-    let mona_dither = dither mona ;;
-      depict mona_dither ;;
-           
+
+depict mona ;;
+
+let mona_threshold = threshold mona 0.75 in
+depict mona_threshold ;;
+
+let mona_dither = dither mona in
+depict mona_dither ;;
